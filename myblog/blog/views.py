8465e0063f930985cf5b538f18d79bb197369b0e
Daewoong Lim <daewoong1025@gmail.com>
2024-05-02 20:40:07 +0900

    Create html templates and render

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post, Comment
 
 # Create your views here.
+def home(request):
+    posts = Post.objects.all()
+
+    context = {
+        'posts':posts,
+        'num_posts':posts.count()
+    }
+
+    return render(request, 'home.html', context=context)
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -23,14 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-urlpatterns += [
-    path('blog/', include('blog.urls')),
-]
-
-urlpatterns += [
     path('', RedirectView.as_view(url='blog/', permanent=True)),
-]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('blog/', include('blog.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
