1363be350abce4005199cc23bf659ccef7dab914
Daewoong Lim <daewoong1025@gmail.com>
2024-05-03 20:39:52 +0900

    Render homepage as class based view

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Post, Comment
 
 # Create your views here.
-def home(request):
-    posts = Post.objects.all()
+# def home(request):
+#     posts = Post.objects.all()
 
-    context = {
-        'posts':posts,
-        'num_posts':posts.count()
-    }
+#     context = {
+#         'posts':posts,
+#         'num_posts':posts.count()
+#     }
 
-    return render(request, 'home.html', context=context)
+#     return render(request, 'home.html', context=context)
+
+class PostListView(generic.ListView):
+    model = Post
+    context_object_name = 'posts'   # your own name for the list as a template variable
+    # queryset = Post.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    template_name = 'home.html'  # Specify your own template name/location
