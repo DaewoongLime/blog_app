d44d39bb39ff5d8fcb111bab9c22a47604b44a43
Daewoong Lim <daewoong1025@gmail.com>
2024-05-09 20:20:40 +0900

    Change DateField to DateTimeField in both models

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
+from datetime import datetime    
 
 class Post(models.Model):
     # model representing a blog post
@@ -7,18 +8,16 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     writer = models.CharField(max_length=100)
     content = models.TextField(max_length=5000)
-    date = models.DateField()
-    likes = models.IntegerField()
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         ordering = ['-date']
 
     def __str__(self):
-        # String for representing the Model object.
         return self.title
     
     def get_absolute_url(self):
-        # Returns the URL to access a detail record for this book.
         return reverse('blog-detail', args=[str(self.id)])
 
 class Comment(models.Model):
@@ -27,17 +26,16 @@ class Comment(models.Model):
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
     writer = models.CharField(max_length=100)
     content = models.CharField(max_length=200)
-    date = models.DateField()
-    likes = models.IntegerField()
-    dislikes = models.IntegerField()
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
 
     class Meta:
-        ordering = ['likes', 'dislikes']
+        ordering = ['-date']
         
     def __str__(self):
         # String for representing the Model object.
         return str(self.post)+" "+self.writer
     
     def get_absolute_url(self):
-        # Returns the URL to access a detail record for this book.
         return reverse('comment-detail', args=[str(self.id)])
