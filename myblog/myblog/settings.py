72fe5719847780f71bab677a854a0a9e7a56237d
Daewoong Lim <daewoong1025@gmail.com>
2024-05-18 16:27:13 +0900

    Use environment variable

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,15 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-3i8qifua&$exw@f0he=3bor7h&5p6*qa4^6=@3(v)d$tyizc#_'
-import os
+load_dotenv(find_dotenv())
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ['DEBUG'] == 'True')
 
 ALLOWED_HOSTS = [
+    # '127.0.0.1',
     ".ap-northeast-2.compute.amazonaws.com"
 ]
 
